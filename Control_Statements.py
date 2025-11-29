abc244805641a98c83e93aa0b5251b50da85f82d
student-sayan <sayanroy702388@gmail.com>
2025-11-29 06:45:28 +0000

    voter age Limit

---
+++ Control_Statements.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age = "))
+
+if age >= 18:
+    print("You are eligible for vote")
+elif age < 1:
+    print("Plece enter the currect age")
+else:
+    print("You are not Eligible for vote")
