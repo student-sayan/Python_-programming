9c947fdfd9c642ee1c3fd7594636265c322925f9
student-sayan <sayanroy702388@gmail.com>
2025-11-29 05:45:53 +0000

    Sum types of operators in python

---
+++ Operators.py
@@ -0,0 +1,49 @@
+"""result = 10+2*3
+print(result)"""
+
+#Arithmetic Operators=====
+
+#x = 10
+#y = 20
+#print(x+y)
+#print(x-y)
+#print(x*y)
+#print(x/y)
+#print(x//y) #floor division(remove the Decimal values)
+#print(x%y) #modulas operator
+#print(x**y)
+
+#Comparison Operators======
+
+#x = 10
+#y = 5
+#
+#print(x == y)
+#print(x != y)
+#print(x > y)
+#print(x < y)
+#print(x >= y)
+
+#Logical Operators======
+
+#a = 10
+#b = 20
+#print(a>5 and b>10)
+#print(a>11 or b<30)
+#print(not (a==10))
+
+#Assignment Operators=============
+
+#x = 10
+#x += 5
+#x -= 5
+#x *= 5
+#print(x)
+
+# Identity operator===========
+a = [1,2,3,4]
+b = a
+c = [1,2,3,4]
+
+print(a is b)
+print(a is c)
